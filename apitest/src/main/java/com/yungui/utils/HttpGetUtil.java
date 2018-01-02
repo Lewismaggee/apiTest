c5b11e9061fbe35353700be45791db4ab7d40b70
Administrator <Administrator@10.1.206.240>
2018-01-02 14:10:27 +0800

    add httpGet request

---
+++ apitest/src/main/java/com/yungui/utils/HttpGetUtil.java
@@ -0,0 +1,39 @@
+package com.yungui.utils;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class HttpGetUtil {
+	private CloseableHttpClient httpClient;
+	private CookieStore cookieStore;
+	
+	public HttpGetUtil() {
+		cookieStore = new BasicCookieStore();
+		httpClient = HttpClients.custom().build();
+	}
+	
+	public String doGet(String url) {
+		HttpGet get = new HttpGet(url);
+		String responseStr = null;
+		CloseableHttpResponse response = null;
+		try {
+			response = httpClient.execute(get);
+			HttpEntity entity = response.getEntity();
+			responseStr = EntityUtils.toString(entity);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return responseStr;
+	}
+}
